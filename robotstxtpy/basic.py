a64b5d66fbadc836d402649054923395eb6af179
Adrian Yeung <adrian.yeung@cern.ch>
2020-01-11 15:41:49 -0800

    Changed annoying yes and no to y and n. Added first \ by default.

--- robotstxtpy/basic.py
+++ robotstxtpy/basic.py
@@ -4,17 +4,17 @@ import os
 
 def writeInput():
     ep = input("Enter endpoint.\n")
-    st = input("Allow or disallow. Enter yes or no.\n")
-    if st == 'yes':
+    st = input("Allow or disallow. Enter y or n.\n")
+    if st == 'y':
         flag = 1
-    elif st == 'no':
+    elif st == 'n':
         flag = 0
     else:
         print("invalid response")
         writeInput()
 
 
-    allow = {1: "Allow: ", 0: "Disallow: "}
+    allow = {1: "Allow: /", 0: "Disallow: /"}
     # if file not exist
     # write User-agent: *
     myfile = open("endpoints.txt", "a+")
@@ -32,8 +32,8 @@ def main():
     stop = 0
     while not stop:
         writeInput()
-        st = input("stop? yes or no.\n")
-        if st == "yes":
+        st = input("stop? Enter y or n.\n")
+        if st == "y":
             stop = 1
         else:
             pass
