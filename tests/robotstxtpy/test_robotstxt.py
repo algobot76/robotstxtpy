a9d2c5ab0a0f216ed80070574fd36a6fa4d885e5
Kaitian Xie <xkaitian@gmail.com>
2020-01-11 14:58:16 -0800

    Add tests

---
+++ tests/robotstxtpy/test_robotstxt.py
@@ -0,0 +1,23 @@
+import tests.utils as test_utils
+from robotstxtpy import RobotsTxt
+
+
+def test_robotstxt():
+    foo = RobotsTxt()
+    assert foo.content == dict()
+
+    foo.add_user_agent("Chrome")
+    assert foo.content == {'Chrome': []}
+
+    foo.add_endpoint('Chrome', 'Allow', '/foo')
+    assert foo.content == {'Chrome': [('Allow', '/foo')]}
+
+    foo.add_endpoint('FireFox', 'Disallow', '/bar')
+    assert foo.content == {'Chrome': [('Allow', '/foo')],
+                           'FireFox': [('Disallow', '/bar')]}
+
+    test_utils.assert_same_elements_in_lists(foo.user_agents,
+                                             ['Chrome', 'FireFox'])
+    test_utils.assert_same_elements_in_lists(foo.endpoints, [('Allow', '/foo'),
+                                                             ('Disallow',
+                                                              '/bar')])
