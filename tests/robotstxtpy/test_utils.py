16a6c0832afc995ec91345a4806641b2a4b544dc
omar2535 <omar2535@gmail.com>
2020-01-12 00:22:27 -0800

    adding crawler cli and some utilities

--- tests/robotstxtpy/test_utils.py
+++ tests/robotstxtpy/test_utils.py
@@ -11,3 +11,10 @@ def test_validate_url(mocker, example_valid_url, example_invalid_url):
     mocker.patch.object(utils, '__is_reachable_url',
                         return_value=False)
     assert not utils.validate_url(example_valid_url)
+
+
+def test_filter_out_path_from_url():
+    assert 'google.ca' == utils.filter_out_path_from_url('https://google.ca')
+    assert 'random/stuff.com' == utils.filter_out_path_from_url(
+        'http://www.random/stuff.com')
+    assert 'op.gg' == utils.filter_out_path_from_url('www.op.gg')
