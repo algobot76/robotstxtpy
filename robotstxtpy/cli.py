7e8ee88364b82aff7b5c506a75a6daeb1c5a5a05
Kaitian Xie <xkaitian@gmail.com>
2020-01-12 10:28:16 -0800

    Add setup.py and refactor cli.py

--- robotstxtpy/cli.py
+++ robotstxtpy/cli.py
@@ -12,45 +12,45 @@ def add_user_agent(agent):
         return
     else:
         click.echo('User agent is %s' % agent)
-        allowed_url()
+        add_allowed_url()
 
 
 @click.command()
-@click.option('--allow_url', prompt='Enter "Allow" endpoints or "n" to skip',
+@click.option('--allowed_url', prompt='Enter "Allow" endpoints or "n" to skip',
               help='"Allow" endpoints or "n" to skip')
-def allowed_url(allow_url):
-
-    if allow_url == 'exit':
+def add_allowed_url(allowed_url):
+    if allowed_url == 'exit':
         return
-    elif allow_url == 'n':
-        disallowed_url()
-    elif allow_url[:1] == '/':
-        click.echo('endpoint added : %s' % allow_url)
-        allowed_url()
+    elif allowed_url == 'n':
+        add_disallowed_url()
+    elif allowed_url[:1] == '/':
+        click.echo('endpoint added : %s' % allowed_url)
+        add_allowed_url()
     else:
         click.echo('endpoint needs to start with /')
-        allowed_url()
+        add_allowed_url()
 
 
 @click.command()
-@click.option('--disallow', prompt='Enter "Disallow" endpoints or "n" to skip',
-              help='"Disalllow" endpoint or "n" to skip')
-def disallowed_url(disallow):
-    if disallow == 'exit':
+@click.option('--disallowed_url',
+              prompt='Enter "Disallow" endpoints or "n" to skip',
+              help='"Disallow" endpoint or "n" to skip')
+def add_disallowed_url(disallowed_url):
+    if disallowed_url == 'exit':
         return
-    elif disallow == 'n':
+    elif disallowed_url == 'n':
         more_user_agent()
-    elif disallow[:1] == '/':
-        click.echo('Disallow endpoint added : %s' % disallow)
-        disallowed_url()
+    elif disallowed_url[:1] == '/':
+        click.echo('Disallow endpoint added : %s' % disallowed_url)
+        add_disallowed_url()
     else:
         click.echo('endpoint needs to start with /')
-        disallowed_url()
+        add_disallowed_url()
 
 
 @click.command()
 @click.option('--ans', prompt="'y' to add more user-agents "
-              "'n' to produce the robots.txt",
+                              "'n' to produce the robots.txt",
               help="'y' to add more user-agents 'n' to produce the robots.txt")
 def more_user_agent(ans):
     if ans == 'y':
@@ -63,11 +63,3 @@ def more_user_agent(ans):
     else:
         click.echo('that is not a valid answer')
         more_user_agent()
-
-
-# ask for user agent
-# ask permision
-# ask for end point
-
-if __name__ == '__main__':
-    add_user_agent()
