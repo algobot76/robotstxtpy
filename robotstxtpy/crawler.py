1efba56dfb8c09518058c16f7000995206525849
omar2535 <omar2535@gmail.com>
2020-01-12 00:22:27 -0800

    adding crawler cli and some utilities

--- robotstxtpy/crawler.py
+++ robotstxtpy/crawler.py
@@ -1,60 +1,66 @@
-from bs4 import BeautifulSoup 
+from collections import deque
+from urllib.parse import urlsplit
+
 import requests
 import requests.exceptions
-from urllib.parse import urlsplit
-from urllib.parse import urlparse
-from collections import deque
+from bs4 import BeautifulSoup
 
-# takes in a url
-# ie. url = "https://scrapethissite.com"
-def get_endpoints_from_url(url) -> set():
-	new_urls = deque([url])
 
-	# urls that have already been visited
-	processed_urls = set()
+class Crawler:
 
-	# urls that are local to the website
-	local_urls = set()
+    def __init__(self, url):
+        self.url = url
 
-	# urls that are not from the website
-	foreign_urls = set()
+    # takes in a url
+    # ie. url = "https://scrapethissite.com"
+    def get_endpoints_from_url(self) -> set():
+        new_urls = deque([self.url])
 
-	# urls that are broken
-	broken_urls = set()
+        # urls that have already been visited
+        processed_urls = set()
 
-	# go through new urls
-	while len(new_urls):
-		url = new_urls.popleft()
-		processed_urls.add(url)
-		try:
-			response = requests.get(url)
-		except(requests.exceptions.MissingSchema,
-			   requests.exceptions.ConnectionError,
-			   requests.exceptions.InvalidURL,
-			   requests.exceptions.InvalidSchema):
-			broken_urls.add(url)
-			continue
-		parts = urlsplit(url)
-		base = "{0.netloc}".format(parts)
-		strip_base = base.replace("www.", "")
-		base_url = "{0.scheme}://{0.netloc}".format(parts)
-		path = url[:url.rfind('/')+1] if '/' in parts.path else url
-		soup = BeautifulSoup(response.text, "lxml")
-		for link in soup.find_all('a'):
-			# extract link url from the anchor
-			anchor = link.attrs["href"] if "href" in link.attrs else ''
-			if anchor.startswith('/'):
-				local_link = base_url + anchor
-				local_urls.add(local_link)
-			elif strip_base in anchor:
-				local_urls.add(anchor)
-			elif not anchor.startswith('http'):
-				local_link = path + anchor
-				local_urls.add(local_link)
-			else:
-				foreign_urls.add(anchor)
-			for i in local_urls:
-				if not i in new_urls and not i in processed_urls: 
-					new_urls.append(i)
+        # urls that are local to the website
+        local_urls = set()
 
-	return local_urls
+        # urls that are not from the website
+        foreign_urls = set()
+
+        # urls that are broken
+        broken_urls = set()
+
+        # go through new urls
+        while len(new_urls):
+            url = new_urls.popleft()
+            processed_urls.add(url)
+            try:
+                response = requests.get(url)
+            except(requests.exceptions.MissingSchema,
+                    requests.exceptions.ConnectionError,
+                    requests.exceptions.InvalidURL,
+                    requests.exceptions.InvalidSchema):
+                broken_urls.add(url)
+                continue
+            parts = urlsplit(url)
+            base = '{0.netloc}'.format(parts)
+            strip_base = base.replace('www.', '')
+            base_url = '{0.scheme}://{0.netloc}'.format(parts)
+            path = url[:url.rfind('/')+1] if '/' in parts.path else url
+            soup = BeautifulSoup(response.text, 'lxml')
+            for link in soup.find_all('a'):
+                # extract link url from the anchor
+                anchor = link.attrs['href'] if 'href' in link.attrs else ''
+                if anchor.startswith('/'):
+                    local_link = base_url + anchor
+                    local_urls.add(local_link)
+                elif strip_base in anchor:
+                    local_urls.add(anchor)
+                elif not anchor.startswith('http'):
+                    local_link = path + anchor
+                    local_urls.add(local_link)
+                else:
+                    foreign_urls.add(anchor)
+                for i in local_urls:
+                    if i not in new_urls and i not in processed_urls:
+                        new_urls.append(i)
+
+        return local_urls
