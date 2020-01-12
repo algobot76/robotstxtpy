c1f9e82dfc8ad65e9bb29c10785964f371b07bf7
Aaron <Aaron@As-MacBook-Pro.local>
2020-01-12 11:39:13 -0800

    added crawler executable

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='robotstxt',
-    version='0.1',
+    version='0.0.2',
+    author="Example Author",
+    url="https://github.com/algobot76/robotstxtpy",
     include_package_data=True,
-    install_requires=[
-        'click',
-    ],
     entry_points='''
         [console_scripts]
         robotstxt=robotstxtpy.cli:add_user_agent
+        crawler=robotstxtpy.crawler_cli:add_user_agent
     ''',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  
+    install_requires=[
+        'beautifulsoup4',
+        'certifi',
+        'chardet',
+        'click',
+        'Flask',
+        'idna',
+        'itsdangerous',
+        'Jinja2',
+        'MarkupSafe',
+        'pkg-resources',
+        'requests',
+        'soupsieve',
+        'urllib3',
+        'Werkzeug',
+    ],
+    python_requires='>=3.6',
 )
